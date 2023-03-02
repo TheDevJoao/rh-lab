75944d8afc25f34692c13b2cadb733264f073e38
thedevjoao <thejoao94@gmail.com>
2023-03-02 17:54:25 -0300

    feat: tests the active_employees_getter service

---
+++ spec/services/active_employees_getter_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ActiveEmployeesGetter do
+  describe '#call' do
+    let!(:john) { create(:employee, :with_active_employment) }
+    let!(:jane) { create(:employee, :with_inactive_employment) }
+
+    context 'when employed' do
+      subject { ActiveEmployeesGetter.new.call }
+
+      it { is_expected.to include(john) }
+    end
+
+    context 'when not employed' do
+      subject { ActiveEmployeesGetter.new.call }
+
+      it { is_expected.not_to include(jane) }
+    end
+  end
+end
