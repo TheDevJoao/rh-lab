103befb7dd6bb86a423bde3dcf24d5f932ca302c
thedevjoao <thejoao94@gmail.com>
2023-03-03 13:13:33 -0300

    refactor: renames the active_employee service
    - changed from active_employees_getter to active_employees_fetcher
    - refactors the test file name, describe and subject method.

--- app/services/active_employees_fetcher.rb
+++ app/services/active_employees_fetcher.rb
@@ -1,4 +1,4 @@
-class ActiveEmployeesGetter
+class ActiveEmployeesFetcher
   def call
     Employee.includes(:employments).where(employments: { ends_on: nil })
   end
--- spec/services/active_employees_fetcher_spec.rb
+++ spec/services/active_employees_fetcher_spec.rb
@@ -2,19 +2,19 @@
 
 require 'rails_helper'
 
-RSpec.describe ActiveEmployeesGetter do
+RSpec.describe ActiveEmployeesFetcher, type: :service do
   describe '#call' do
     let!(:john) { create(:employee, :with_active_employment) }
     let!(:jane) { create(:employee, :with_inactive_employment) }
 
     context 'when employed' do
-      subject { ActiveEmployeesGetter.new.call }
+      subject { described_class.new.call }
 
       it { is_expected.to include(john) }
     end
 
     context 'when not employed' do
-      subject { ActiveEmployeesGetter.new.call }
+      subject { described_class.new.call }
 
       it { is_expected.not_to include(jane) }
     end
